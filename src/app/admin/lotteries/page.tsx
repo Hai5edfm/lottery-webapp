bd533af5fce7af2fb4b69e719fc4c145610b80a9
hai5edfm <haisedfm@hai5e.com>
2024-03-15 08:55:52 -0600

    add admin/lotteries

--- src/app/admin/lotteries/page.tsx
+++ src/app/admin/lotteries/page.tsx
@@ -1,7 +1,69 @@
+"use client"
+import { useState } from "react"
 import { type NextPage } from "next"
+import Image from "next/image"
+import { IoAddOutline } from "react-icons/io5"
+
+import fly_icon from "@/assets/images/fly.png"
+import guitar_icon from "@/assets/images/guitar.png"
+import { LotteryCardsList } from "@/modules/ui/components/cards/lottery-cards-list"
+import { NavBar } from "@/modules/ui/components/nav-bar"
+import { Search } from "@/modules/ui/components/search-bar/search"
+import { Tabs } from "@/modules/ui/components/tabs"
+import { ADMIN_TABS } from "@/modules/utils/constants"
+import { LOTTERIES } from "@/modules/utils/constants/lottery"
 
 const LotteriesPage: NextPage = () => {
-  return <div>LotteriesPage</div>
+  const [selectedTab, setSelectedTab] = useState("All")
+
+  return (
+    <main className="min-h-screen bg-[#272961]">
+      <NavBar />
+
+      <section className="m-auto flex max-w-4xl flex-col items-center justify-center py-10 lg:w-[80%]">
+        <div className="w-[86%]">
+          <div className="flex justify-between gap-4">
+            <div className="w-full">
+              <Search />
+            </div>
+            <button
+              type="button"
+              className="flex w-4/12 items-center rounded-lg bg-[#EF5656] px-4 py-2 font-semibold hover:bg-[#e25959]"
+            >
+              Add new lottery
+              <IoAddOutline size={32} />
+            </button>
+          </div>
+
+          <div className="mt-24 hidden md:block md:px-2">
+            <Tabs
+              tabsArray={ADMIN_TABS}
+              selectedTab={selectedTab}
+              setSelectedTab={setSelectedTab}
+            />
+          </div>
+          <div className="my-10 grid grid-cols-1 gap-4 gap-y-6 md:grid-cols-2 lg:grid-cols-3">
+            <LotteryCardsList lotteries={LOTTERIES} />
+          </div>
+        </div>
+      </section>
+
+      <Image
+        alt=""
+        src={fly_icon}
+        width={200}
+        height={200}
+        className="absolute bottom-12 right-0 hidden xl:block"
+      />
+      <Image
+        alt=""
+        src={guitar_icon}
+        width={200}
+        height={200}
+        className="absolute left-0 top-80 hidden xl:block"
+      />
+    </main>
+  )
 }
 
 export default LotteriesPage
--- src/modules/utils/constants/tabs.ts
+++ src/modules/utils/constants/tabs.ts
@@ -15,3 +15,26 @@ export const TABS = [
     onClick: () => {},
   },
 ]
+
+export const ADMIN_TABS = [
+  {
+    name: "All",
+    selected: true,
+    onClick: () => {},
+  },
+  {
+    name: "Created by me",
+    selected: false,
+    onClick: () => {},
+  },
+  {
+    name: "Public",
+    selected: false,
+    onClick: () => {},
+  },
+  {
+    name: "Private",
+    selected: false,
+    onClick: () => {},
+  },
+]
