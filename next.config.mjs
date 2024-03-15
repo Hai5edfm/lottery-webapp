59272f96c7d6809a70c6ed1cff12d968b629ec8d
Kevin Dev <kevin.developer71@gmail.com>
2024-03-14 20:42:15 -0600

    build: validate env vars and optimize build time

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { env } from "./src/modules/config/env.mjs"
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
